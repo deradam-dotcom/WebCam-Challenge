d3fe1d734f6cbb68ba74e3b2327a69e39e6b4dec
deradam-dotcom <deradam1@gmail.com>
2021-06-08 19:11:46 +0200

    WebCamReady

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
   let [cities, setCities] = useState([]);
   let [cam, setCam] = useState("");
   let [showControl, setShowControl] = useState(false);
-  let [xAssis, setXassis] = useState(0);
-  let [yAssis, setYassis] = useState(0);
+  // every timne updating the pos with the new position //
+  let [pos, setPos] = useState("0px 0px");
+  let [isMouseDown, setIsMouseDown] = useState(false);
+  let [startingPos, setStartingPos] = useState({ x: 0, y: 0 });
+  let [endingPos, setEndingPos] = useState({ x: 0, y: 0 });
+  let [currentPos, setCurrentPos] = useState({ x: 0, y: 0 });
+  const [pictureWidth, setPictureWidth] = useState(0);
+  const [pictureHeight, setPictureHeight] = useState(0);
+  const [cameraViewWidth, setCameraViewWidth] = useState(0);
+  const [cameraViewHeight, setCameraViewHeight] = useState(0);
+  const ref = useRef(null);
+  useEffect(() => {
+    //"cameViewWith & Height" are necessary to keep the selected camera picture inside the "cameraView" div during the moving
+    setCameraViewWidth(ref.current ? ref.current.offsetWidth : 0);
+    setCameraViewHeight(ref.current ? ref.current.offsetHeight : 0);
+    const img = new Image();
+    img.onload = function () {
+      //  "pictureWidth & Height" are necessary to keep the selected camera view inside the div
+      setPictureWidth(this.width);
+      setPictureHeight(this.height);
+    };
+    img.src = cam;
+  }, [cam]);
+
   let showCam = (indx) => {
     let newCam = cam;
     cities.map((citie, indexC) => {
@@ -15,6 +37,7 @@ function App() {
       }
     });
     setCam(newCam);
+    // setting the starting position of the new selected camera's view //
   };
 
   let getControl = (camera) => {
@@ -24,15 +47,43 @@ function App() {
   let getCameras = (camera) => {
     setCam(camera);
     setShowControl(false);
+    let newPos = endingPos;
+    newPos = { x: 0, y: 0 };
+    setEndingPos(newPos);
   };
 
-  let startingPoint = (e) => {
-    let x = e.movementX;
-    let y = e.movementY;
-    setXassis(x);
-    setYassis(y);
+  let setPositionDown = (e) => {
+    let strPos = startingPos;
+    strPos = { x: e.clientX, y: e.clientY };
+    setStartingPos(strPos);
+    setIsMouseDown(true);
   };
 
+  let setPosition = (e) => {
+    let distanceX = e.clientX - startingPos.x + endingPos.x;
+    let distanceY = e.clientY - startingPos.y + endingPos.y;
+    let newDistance = currentPos;
+    newDistance = { x: distanceX, y: distanceY };
+    if (
+      isMouseDown &&
+      newDistance.x <= 0 &&
+      newDistance.y <= 0 &&
+      Math.abs(newDistance.x) + cameraViewWidth <= pictureWidth &&
+      Math.abs(newDistance.y) + cameraViewHeight <= pictureHeight
+    ) {
+      // get the newPos coordinates of the camera//
+      let newPos =
+        newDistance.x.toString() + "px " + newDistance.y.toString() + "px";
+      setCurrentPos(newDistance);
+      setPos(newPos);
+    }
+  };
+  let setPositionUp = (e) => {
+    let endPos = endingPos;
+    endPos = { x: currentPos.x, y: currentPos.y };
+    setEndingPos(endPos);
+    setIsMouseDown(false);
+  };
   useEffect(() => {
     fetch("http://runningios.com/screamingbox/cameras.json")
       .then((res) => res.json())
@@ -47,13 +98,16 @@ function App() {
   return (
     <div className="App">
       <div className="panel-container">
-        <div className="cam-container">
-          <img
-            src={cam}
-            alt="cam"
-            style={{ top: yAssis + "px", left: xAssis + "px" }}
-          />
-        </div>
+        {console.log("**POS/CAM**", pos, cam)}
+        <div
+          className="cam-container"
+          ref={ref}
+          style={{
+            backgroundImage: `url(${cam})`,
+            backgroundPosition: `${pos}`,
+            backgroundRepeat: "no-repeat",
+          }}
+        />
         <div className="cities-container">
           <div className="btn-container">
             <button
@@ -93,7 +147,9 @@ function App() {
               <h3>Click in the circle & Drag</h3>
               <div
                 className="drag-circle"
-                onMouseMove={(e) => startingPoint(e)}
+                onMouseDown={(e) => setPositionDown(e)}
+                onMouseMove={(e) => setPosition(e)}
+                onMouseUp={(e) => setPositionUp(e)}
               ></div>
             </div>
           )}
